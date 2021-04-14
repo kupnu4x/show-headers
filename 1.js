6b4e1ee04b1d73641ac7647d734330e81abb5a49
Dmitry Kamenskikh <kupnu4x@gmail.com>
2021-04-14 13:33:54 +0800

    json structure in answer

--- 1.js
+++ 1.js
@@ -2,11 +2,11 @@
 const http = require('http')
 const port = 3000
 const requestHandler = (request, response) => {
-
-    var ret = request.headers;
-    ret.url = request.url;
-    ret.remote_addr = request.socket.remoteAddress
-    response.end(JSON.stringify(ret, null, 2))
+    response.end(JSON.stringify({
+        headers: request.headers,
+        url: request.url,
+        remote_addr: request.socket.remoteAddress
+    }, null, 2))
     //console.log(request)
 }
 const server = http.createServer(requestHandler)
---
+++ README.md
@@ -0,0 +1,5 @@
+```
+docker build -t kupnu4x/show-headers -t quay.io/kupnu4x/show-headers .
+docker push kupnu4x/show-headers
+docker push quay.io/kupnu4x/show-headers
+```
